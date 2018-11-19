4d2ae244d84f7cfac66873cb2363f4cd4d359b58
percy0 <scoutardilla@gmail.com>
2018-11-19 19:17:11 +0100

    get y set de caballo

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -0,0 +1,26 @@
+package org.iesalandalus.programacion.caballoajedrez;
+
+public class Caballo {
+	
+	private Color color;
+	private Posicion posicion;
+	
+	
+	
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
+	
+	
+	
+}
--- src/test/java/org/iesalandalus/programacion/caballoajedrez/CaballoTest.java
+++ src/test/java/org/iesalandalus/programacion/caballoajedrez/CaballoTest.java
@@ -7,8 +7,8 @@ import javax.naming.OperationNotSupportedException;
 import org.junit.Test;
 
 public class CaballoTest {
-	
-	/*private Posicion posicionDefectoNegro = new Posicion(8, 'b');
+	/*
+	private Posicion posicionDefectoNegro = new Posicion(8, 'b');
 	private Posicion posicionDefectoBlanco = new Posicion(1, 'b');
 	private Posicion posicionNegroG = new Posicion(8, 'g');
 	private Posicion posicionBlancoG = new Posicion(1, 'g');
